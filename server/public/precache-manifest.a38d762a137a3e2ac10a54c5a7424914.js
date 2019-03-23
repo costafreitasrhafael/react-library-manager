1132238d57cde685606e3d986a00ff349091d70e
Rhafael Freitas da Costa <costa.cfr@gmail.com>
2019-03-23 11:31:36 -0300

    Project Build

---
+++ server/public/precache-manifest.a38d762a137a3e2ac10a54c5a7424914.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "88675c1fb7048b99be17",
+    "url": "/static/css/main.5adc4ebe.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7c3c73783d3cf071bf65",
+    "url": "/static/js/2.15d8ad31.chunk.js"
+  },
+  {
+    "revision": "88675c1fb7048b99be17",
+    "url": "/static/js/main.647a7cfa.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7c3c73783d3cf071bf65",
+    "url": "/static/css/2.e3082747.chunk.css"
+  },
+  {
+    "revision": "1f088e0ade766dafa44d1af651f99358",
+    "url": "/index.html"
+  }
+];
